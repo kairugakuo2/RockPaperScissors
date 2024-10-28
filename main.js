5622946a78e559c4327a09db9a051068803b4254
Gakuo <kairugakuo9@gmail.com>
2024-10-28 11:56:46 -0500

    Created function to get the computer choice of either rock, paper, or scissors based on random int values.

---
+++ main.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    let n = Math.floor(Math.random() * 3);
+    let choice;
+    if (n === 0){
+        choice = "rock";
+    } else if (n === 1){
+        choice = "paper";
+    } else if (n === 2){
+        choice = "scissors";
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());

bf2b84046cf5016728dc8d5e0f9aef5ba1a80fe3
Gakuo <kairugakuo9@gmail.com>
2024-10-28 13:04:37 -0500

    Updated playRound to get human and computer selections (human from id of button pressed) and output the winner using whoIsWinner function

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
 function getComputerChoice(){
     let n = Math.floor(Math.random() * 3);
-    let choice;
+    let computerChoice;
     if (n === 0){
-        choice = "rock";
+        computerChoice = "rock";
     } else if (n === 1){
-        choice = "paper";
-    } else if (n === 2){
-        choice = "scissors";
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
     }
-    return choice;
+    return computerChoice;
 }
 
 function getHumanChoice(button){
-    console.log(button.id);
+    return button.id;
 }
 
 
-console.log(getComputerChoice());
+function playRound(button){
+    console.clear();
+    let computerSelection = getComputerChoice();
+    let humanSelection = getHumanChoice(button);
+    const result = whoIsWinner(computerSelection, humanSelection);
+
+    console.log(`Computer chose: ${computerSelection}`);
+    console.log(`You chose: ${humanSelection}`);
+    if (result ==="tie") {
+        console.log("Result: It was a tie!");
+    } else {
+        console.log(`Result: ${result} is the winner!`);
+    }
+
+    return result;
+}
+
+function whoIsWinner(computerSelection, humanSelection){
+    //if there's a tie
+    if(computerSelection === humanSelection){
+        return "tie";
+    }
+    //if computer is the winner
+    if( (computerSelection === "rock" && humanSelection === "scissors") ||
+        (computerSelection === "paper" && humanSelection === "rock") ||
+        (computerSelection === "scissors" && humanSelection === "paper")){
+        return "Computer";
+    } else {
+        return "Human";  // else human is the winner if no tie and computer isn't winner
+    }
+}
